627aa342d00c8eac04570bb8c60eff32ca6b1164
Neelabh Somani <83975955+SedHabibi@users.noreply.github.com>
2021-09-06 20:41:08 +0530

    Update banknotes.py

--- BankNotes/banknotes.py
+++ BankNotes/banknotes.py
@@ -6,9 +6,12 @@ Created on Mon Sep  6 20:32:17 2021
 """
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
 
 # Importing the dataset
 dataset = pd.read_csv('banknotes.csv')
@@ -16,17 +19,14 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
 # Fitting XGBoost to the Training set
-from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
 
 # Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
@@ -34,12 +34,10 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print ("confusion matrix : "+str(cm))
 
 # Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 print ("mean accuracy is : "+str(round(accuracies.mean()*100,2))+"%")
 print ("standard deviation : "+str(accuracies.std()))
